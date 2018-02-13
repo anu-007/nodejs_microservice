fd054de9c585a99fafa2e6a7b3a6c79fc1ee4a94
anu-007 <anubhutiv1@gmail.com>
2018-02-13 15:34:05 +0530

    functional jwt authorization

--- app.js
+++ app.js
@@ -9,19 +9,54 @@ app.get('/', (req, res) => {
     });
 });
 
+app.post('/thumb', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'the lost world', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created',
+                authData
+            });
+        }
+    });
+});
+
 app.post('/login', (req, res) => {
     //Mock user login
     const user = {
-        id: 1,
-        username: 'anu-007',
-        email: 'anubhutiv1@gmail.com'
-    }
+        username: 'tom',
+        password: 'dale'
+    };
 
-    jwt.sign({user}, 'the lost world', (err, token) => {
+    jwt.sign({user}, 'the lost world', { expiresIn: '120s' }, (err, token) => {
         res.json({
             token
         });
     });
 });
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+//Verify token
+function verifyToken(req, res, next) {
+    //get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // check if bearer is undefined
+    if(typeof bearerHeader != 'undefined') {
+        // Split at space
+        const bearer = bearerHeader.split(' ');
+        // get token from array
+        const bearerToken = bearer[1];
+        // set the token
+        req.token = bearerToken;
+        // next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
 
 app.listen(4000, () => console.log('server running on port 4000'));
