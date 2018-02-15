6d523494337bd609f3f54e076f23208201eac45e
anu-007 <anubhutiv1@gmail.com>
2018-02-15 14:24:29 +0530

    linting fixed

--- app.js
+++ app.js
@@ -4,68 +4,20 @@ const sharp = require('sharp');
 const download = require('image-downloader');
 const jsonpatch = require('json-patch');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static('./thumbnails'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'welcome to the microservice: go to /login route and enter username and password'
-    });
-});
-
-app.post('/thumb', setToken, verifyToken, (req, res) => {
-    var options = {
-        url: req.body.url,
-        dest: __dirname + '/thumbnails/'
-    };
-       
-    download.image(options)
-       .then(({ filename, image }) => {
-         sharp(image)
-            .resize(50, 50)
-            .toFile(filename, function() {
-                res.sendFile(filename);
-            });
-       }).catch((err) => {
-         throw err;
-       });
-});
-
-app.post('/ptch', setToken, verifyToken, (req, res) => {
-    const patch = jsonpatch.apply(req.body, [ {op: 'add', path: '/foo', value: 'bar'},
-                                        { op: 'replace', path: '/baz', value: 'boo' },
-                                        { op: 'add', path: '/hello', value: ['world'] },
-                                        { op: 'remove', path: '/foo'}
-                                    ]);
-    res.json(patch);
-});
-
-app.post('/login', (req, res) => {
-    //Mock user login
-    const user = {
-        username: req.body.user,
-        password: req.body.password
-    };
-
-    jwt.sign({user}, 'the lost world', { expiresIn: '120s' }, (err, token) => {
-        res.json({
-            token
-        });
-    });
-});
-
 // FORMAT OF TOKEN
 // Authorization: Bearer <access_token>
 
-//Verify token
 function setToken(req, res, next) {
-    //get auth header value
-    const bearerHeader = req.headers['authorization'];
+    const bearerHeader = req.headers.authorization;
     // check if bearer is undefined
-    if(typeof bearerHeader != 'undefined') {
+    if (typeof bearerHeader !== 'undefined') {
         // Split at space
         const bearer = bearerHeader.split(' ');
         // get token from array
@@ -90,6 +42,53 @@ function verifyToken(req, res, next) {
     });
 }
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'welcome to the microservice: go to /login route and enter username and password'
+    });
+});
+
+app.post('/thumb', setToken, verifyToken, (req, res) => {
+    const options = {
+        url: req.body.url,
+        dest: path.join(__dirname, '/thumbnails/'),
+    };
+    download.image(options)
+       .then(({ filename, image }) => {
+         sharp(image)
+            .resize(50, 50)
+            .toFile(filename, () => {
+                res.sendFile(filename);
+            });
+       }).catch((err) => {
+         throw err;
+       });
+});
+
+app.post('/ptch', setToken, verifyToken, (req, res) => {
+    const patch = jsonpatch.apply(req.body, [{ op: 'add', path: '/foo', value: 'bar' },
+                                        { op: 'replace', path: '/baz', value: 'boo' },
+                                        { op: 'add', path: '/hello', value: ['world'] },
+                                        { op: 'remove', path: '/foo' },
+                                    ]);
+    res.json(patch);
+});
+
+app.post('/login', (req, res) => {
+    const user = {
+        username: req.body.user,
+        password: req.body.password,
+    };
+
+    jwt.sign({ user }, 'the lost world', { expiresIn: '120s' }, (err, token) => {
+        res.json({
+            token,
+        });
+    });
+});
+
 module.exports = app;
 
-app.listen(4000, () => {console.log('server running on port 4000');});
+app.listen(4000, () => {
+    console.log('server running on port 4000');
+});
--- test/apptest.js
+++ test/apptest.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
-const should = chai.should();
+
+chai.should();
 
 chai.use(chaiHttp);
 
 describe('Thumbnail microservice Testing', () => {
-
     describe('Testing for welcome message', () => {
         it('it should GET a welcome message', (done) => {
             chai.request(server)
@@ -21,13 +21,12 @@ describe('Thumbnail microservice Testing', () => {
     });
 
     describe('Testing for public routes', () => {
-        //include test cases for empty username or password pair
         it('it returns JWT token for a username and password pair', () => {
             chai.request(server)
                 .post('/login')
                 .type('json')
                 .send('{"username":"admin","password":"admin"}')
-                .end(function(err, res) {
+                .end((err, res, done) => {
                     if (err) return done(err);
                     res.body.should.have.property('jwt');
                 done();
@@ -36,20 +35,20 @@ describe('Thumbnail microservice Testing', () => {
     });
 
     describe('testing for protected routes', () => {
-        var token = '', result;
-        beforeEach(function(done) {
+        let tokenjwt = '';
+        beforeEach((done) => {
             chai.request(server)
               .post('/login')
               .type('json')
               .send('{"username":"admin","password":"admin"}')
-              .end(function(err, res) {
-                result = JSON.parse(res.text);
-                token = result.token;
+              .end((err, res) => {
+                const result = JSON.parse(res.text);
+                tokenjwt = result.token;
                 done();
             });
         });
 
-        it('should not be able to consume the route /thumb since no token was sent', function(done) {
+        it('should not be able to consume the route /thumb since no token was sent', (done) => {
             chai.request(server)
               .post('/thumb')
               .end((err, res) => {
@@ -58,29 +57,29 @@ describe('Thumbnail microservice Testing', () => {
             });
         });
 
-        it('should not be able to consume the route /thumb since unauthorized token was sent', function(done) {
+        it('should not be able to consume the route /thumb since unauthorized token was sent', (done) => {
             chai.request(server)
               .post('/thumb')
-              .set('Authorization', 'Bearer ' + 'some other token')
+              .set('Authorization', 'Bearer someothertoken')
               .end((err, res) => {
                 res.should.have.status(403);
                 done();
             });
         });
 
-        it('should be able to consume the route /thumb since token valid was sent', function(done) {
+        it('should be able to consume the route /thumb since token valid was sent', (done) => {
             chai.request(server)
               .post('/thumb')
               .type('json')
               .send('{"url":"https://cdn.pixabay.com/photo/2017/07/29/20/18/minions-2552584_960_720.jpg"}')
-              .set('Authorization', 'Bearer ' + token)
+              .set('Authorization', `Bearer ${tokenjwt}`)
               .end((err, res) => {
                 res.should.have.status(200);
                 done();
             });
         });
 
-        it('should not be able to consume the route /ptch since no token was sent', function(done) {
+        it('should not be able to consume the route /ptch since no token was sent', (done) => {
             chai.request(server)
               .post('/ptch')
               .end((err, res) => {
@@ -89,22 +88,22 @@ describe('Thumbnail microservice Testing', () => {
             });
         });
 
-        it('should not be able to consume the route /ptch since unauthorized token was sent', function(done) {
+        it('should not be able to consume the route /ptch since unauthorized token was sent', (done) => {
             chai.request(server)
               .post('/ptch')
-              .set('Authorization', 'Bearer ' + 'some other token')
+              .set('Authorization', 'Bearer someothertoken')
               .end((err, res) => {
                 res.should.have.status(403);
                 done();
             });
         });
 
-        it('should be able to consume the route /ptch since token valid was sent', function(done) {
+        it('should be able to consume the route /ptch since token valid was sent', (done) => {
             chai.request(server)
               .post('/ptch')
               .type('json')
               .send('{"baz":"qux","foo":"bar"}')
-              .set('Authorization', 'Bearer ' + token)
+              .set('Authorization', `Bearer ${tokenjwt}`)
               .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
@@ -112,5 +111,4 @@ describe('Thumbnail microservice Testing', () => {
               });
         });
     });
-
 });
